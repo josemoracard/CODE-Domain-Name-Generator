83051d46453ebd490730416a894f70135ec7ada7
josemoracard <jose.cardinales@gmail.com>
2022-12-13 01:37:45 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let x = 0; x < pronoun.length; x++) {
+  for (let y = 0; y < adj.length; y++) {
+    for (let z = 0; z < noun.length; z++) {
+      console.log(pronoun[x] + adj[y] + noun[z] + ".com");
+    }
+  }
+}
